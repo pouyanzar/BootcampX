8f69ea29754cbb91c19c11b3d07e6b9378b3381a
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-08-17 23:49:30 +0000

    run a query to get  the name of all teachers that performed an assistance request and number of assistances during a cohort.

---
+++ 4_queries/name_teachers_number_assistances.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) AS total_assistances
+FROM cohorts
+JOIN students
+ON cohorts.id = cohort_id
+JOIN assistance_requests
+ON student_id = students.id
+JOIN teachers
+ON teachers.id = teacher_id
+WHERE cohorts.name = 'JUL02' GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
