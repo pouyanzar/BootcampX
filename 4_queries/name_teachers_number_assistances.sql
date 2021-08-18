370abacfe4643194576d1829279ab9972d717b68
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-08-18 00:19:38 +0000

    removed DISTINCT and restructured

--- 4_queries/name_teachers_number_assistances.sql
+++ 4_queries/name_teachers_number_assistances.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) AS total_assistances
+SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) AS total_assistances
 FROM cohorts
 JOIN students
 ON cohorts.id = cohort_id
@@ -6,5 +6,6 @@ JOIN assistance_requests
 ON student_id = students.id
 JOIN teachers
 ON teachers.id = teacher_id
-WHERE cohorts.name = 'JUL02' GROUP BY teachers.name, cohorts.name
+WHERE cohorts.name = 'JUL02' 
+GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
