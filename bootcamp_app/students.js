282f72d76ff1c86d9c7ce968b9f18fd7471f79b8
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-08-19 17:14:22 +0000

    update students query to use paramerterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 const args = process.argv;
-
-pool.query(`
+const queryString = 
+`
 SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts
 ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${args[2]}%'
-LIMIT ${args[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+const cohortName = args[2];
+const limit = args[3] || 5;
+const values = [`%${cohortName}%`, limit];
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
